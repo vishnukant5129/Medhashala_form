93a3c3462d703dfb103a3b5aed3ff6c72b808db9
vishnukant <vishnukantray10@gmail.com>
2026-07-02 12:45:00 +0530

    feat: add prtected route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,41 +1,48 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home.jsx";
-import SurveyForm from "../pages/SurveyForm.jsx";
+import Login from "../pages/Login.jsx";
+import Home from "../pages/Home";
+import SurveyForm from "../pages/SurveyForm";
 import Dashboard from "../pages/Dashboard";
 import Payment from "../pages/payment";
 
-// layouts
 import UserLayout from "../layouts/UserLayout";
 import AdminLayout from "../admin/layout/AdminLayout";
 
-// admin pages (abhi simple example)
 import AdminDashboard from "../admin/pages/AdminDashboard";
 import AdminStudents from "../admin/pages/AdminStudents";
 import AdminReferrals from "../admin/pages/AdminReferrals";
 import AdminPayments from "../admin/pages/AdminPayments";
-import Login from "../pages/login.jsx";
+
+import ProtectedRoute from "./ProtectedRoute";
+import Signup from "../pages/Signup.jsx";
 
 const AppRoutes = () => {
     return (
         <Routes>
 
-            {/* <Route path="/login" element={<Login/>}/> */}
+            {/* Public Route */}
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-            {/* ================= USER SIDE ================= */}
-            <Route element={<UserLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/survey" element={<SurveyForm />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/payment" element={<Payment />} />
+            {/* Protected User Routes */}
+            <Route element={<ProtectedRoute />}>
+                <Route element={<UserLayout />}>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/survey" element={<SurveyForm />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/payment" element={<Payment />} />
+                </Route>
             </Route>
 
-            {/* ================= ADMIN SIDE ================= */}
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route index element={<AdminDashboard />} />
-                <Route path="student" element={< AdminStudents />} />
-                <Route path="referrals" element={<AdminReferrals />} />
-                <Route path="payments" element={< AdminPayments />} />
+            {/* Protected Admin Routes */}
+            <Route element={<ProtectedRoute />}>
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<AdminDashboard />} />
+                    <Route path="student" element={<AdminStudents />} />
+                    <Route path="referrals" element={<AdminReferrals />} />
+                    <Route path="payments" element={<AdminPayments />} />
+                </Route>
             </Route>
 
         </Routes>
