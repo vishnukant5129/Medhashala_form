34fffd93f1a8ce4c2d7d4d190f076a6c566431c9
vishnukant <vishnukantray10@gmail.com>
2026-07-04 00:18:05 +0530

    remove logina and sigup routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
-import Login from "../pages/Login.jsx";
+// import Login from "../pages/Login.jsx";
 import Home from "../pages/Home";
-import SurveyForm from "../pages/SurveyForm";
+import SurveyForm from "../pages/SurveyForm.jsx";
 import Dashboard from "../pages/Dashboard";
 import Payment from "../pages/payment";
 
@@ -14,36 +14,36 @@ import AdminStudents from "../admin/pages/AdminStudents";
 import AdminReferrals from "../admin/pages/AdminReferrals";
 import AdminPayments from "../admin/pages/AdminPayments";
 
-import ProtectedRoute from "./ProtectedRoute";
-import Signup from "../pages/Signup.jsx";
+// import ProtectedRoute from "./ProtectedRoute";
+// import Signup from "../pages/Signup.jsx";
 
 const AppRoutes = () => {
     return (
         <Routes>
 
             {/* Public Route */}
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            {/* <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} /> */}
 
             {/* Protected User Routes */}
-            <Route element={<ProtectedRoute />}>
+            {/* <Route element={<ProtectedRoute />}> */}
                 <Route element={<UserLayout />}>
                     <Route path="/home" element={<Home />} />
                     <Route path="/survey" element={<SurveyForm />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/payment" element={<Payment />} />
                 </Route>
-            </Route>
+            {/* </Route> */}
 
             {/* Protected Admin Routes */}
-            <Route element={<ProtectedRoute />}>
+            {/* <Route element={<ProtectedRoute />}> */}
                 <Route path="/admin" element={<AdminLayout />}>
                     <Route index element={<AdminDashboard />} />
                     <Route path="student" element={<AdminStudents />} />
                     <Route path="referrals" element={<AdminReferrals />} />
                     <Route path="payments" element={<AdminPayments />} />
                 </Route>
-            </Route>
+            {/* </Route> */}
 
         </Routes>
     );
